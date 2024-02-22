e2299527779ff0289dca90c5df9280538f6a42b7
abdullah97-pro <abdullahahmadi019@gmail.com>
2024-02-22 07:26:50 +0430

    disable onhover mouse from particles

--- js/app.js
+++ js/app.js
@@ -85,7 +85,7 @@ particlesJS('snow-fall',
       "detect_on": "canvas",
       "events": {
         "onhover": {
-          "enable": true,
+          "enable": false,
           "mode": "repulse"
         },
         "onclick": {
